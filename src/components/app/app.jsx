5ad441f8b65e21e86deafe6cdb89d4a2b4a1c47b
left38331 <left38331@mail.ru>
2019-10-16 10:09:37 +0300

    dell props

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,11 +1,6 @@
 import React from 'react';
 import {WelcomeScreen} from './../welcome-screen/welcome-screen';
 
-export const App = (props) => {
-  const {gameTime, errorCount} = props;
-
-  return <WelcomeScreen
-    time = {gameTime}
-    errorCount = {errorCount}
-  />;
+export const App = () => {
+  return <WelcomeScreen/>;
 };
--- src/index.js
+++ src/index.js
@@ -3,16 +3,8 @@ import ReactDOM from 'react-dom';
 import {App} from './components/app/app';
 
 const init = () => {
-  const settings = {
-    gameTime: 5,
-    errorCount: 3,
-  };
-
   ReactDOM.render(
-      <App
-        errorCount = {settings.errorCount}
-        gameTime = {settings.gameTime}
-      />,
+      <App/>,
       document.querySelector(`#root`)
   );
 };
