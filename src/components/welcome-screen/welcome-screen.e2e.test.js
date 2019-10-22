f49afa82e322ebbda1b81a0d96f97b8d6aa266cf
left38331 <left38331@mail.ru>
2019-10-22 10:59:33 +0300

    add setup-test.js (basic setup configuration) for all *.e2e.test.js files

--- src/components/welcome-screen/welcome-screen.e2e.test.js
+++ src/components/welcome-screen/welcome-screen.e2e.test.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import Enzyme, {shallow} from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
 import {WelcomeScreen} from "./welcome-screen";
-
-Enzyme.configure({adapter: new Adapter()});
+import {shallow} from 'enzyme';
 
 it(`Test click on button`, () => {
   const clickHandler = jest.fn();
